fc7aa09a1a8fde8efa3f15d0ca3190dc670cd79e
spasquesi <spasquesi@vailsys.com>
2018-07-22 02:35:56 -0500

    changed twitter post to a get

--- stockbuzz/js/twitterFetch.js
+++ stockbuzz/js/twitterFetch.js
@@ -3,10 +3,11 @@ function twitterFetch(){
 
   var t = document.getElementById('ticker').value;
   var name = new Stock().requestStockQuote(t).name;
+  var params = "name="+name
 
   var request = new XMLHttpRequest();
 
-  request.open("POST", url, true);
+  request.open("GET", url+"?"+params, true);
   request.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
   request.send(JSON.stringify({'search': name}));
 
